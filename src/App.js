facc8329ca53831179a3aa8a6034c33358b83c68
iffinc-ga <winnie.yim@iffusa.com>
2025-08-14 16:39:46 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PartsSearchTool from './components/PartsSearchTool';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <PartsSearchTool />
+    </div>
+  );
+}
+
+export default App;
